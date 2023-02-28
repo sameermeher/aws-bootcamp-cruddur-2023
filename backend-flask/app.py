89b3abfaf9c4d2f74c8dcdf7dc3c3f9e387bd818
Sameer Meher <sam.tiku@gmail.com>
2023-02-28 21:08:07 +0530

    Remove ConsoleSpan, SimpleSpan

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,7 +21,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+#from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # X-Ray distributed tracing
 from aws_xray_sdk.core import xray_recorder
@@ -46,8 +46,8 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 # Show the spans in the STDOUT (console) (useful for debugging)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+#simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
